de0fdc4a90a305ef5a717064e06b9b85d55cfb8f
Nick Tzaperas <nick@nijotz.com>
2013-11-10 20:27:10 -0800

    Create a new db for each test

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 import json
 import socket
+from sqlalchemy import create_engine
 import unittest
 from node import DHTBase
+from models import Node
 
 
 class DHTTest(DHTBase):
@@ -13,12 +15,23 @@ class DHTTest(DHTBase):
 class TestNode(unittest.TestCase):
 
     # Only setup once (don't create a socket server for every test)
-    @classmethod
-    def setUpClass(cls):
-        cls.node1 = DHTTest('node1', 'localhost', 1111)
-        cls.node2 = DHTTest('node2', 'localhost', 1112)
-        cls.node1.start()
-        cls.node2.start()
+    def setUp(self):
+        engine = create_engine('postgresql+psycopg2://localhost:5432/postgres')
+        self.db_conn = engine.connect()
+
+        #postgres doesn't allow create db inside a transaction, commits end them
+        self.db_conn.execute("commit")
+        self.db_conn.execute("create database test_node1")
+        self.db_conn.execute("commit")
+
+        self.db_conn.execute("commit")
+        self.db_conn.execute("create database test_node2")
+        self.db_conn.execute("commit")
+
+        self.node1 = DHTTest('node1', 'localhost', 1111, database='test_node1')
+        self.node2 = DHTTest('node2', 'localhost', 1112, database='test_node2')
+        self.node1.start()
+        self.node2.start()
 
     def test_nodes_can_respond_to_pings(self, numruns=0):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,11 +58,20 @@ class TestNode(unittest.TestCase):
         sock.close()
         self.test_nodes_can_respond_to_pings()
 
+    def tearDown(self):
+        self.node1.stop()
+        self.node2.stop()
 
-    @classmethod
-    def tearDownClass(cls):
-        # TODO: sessions everywhere!
-        cls.node1.session.delete(cls.node1.node)
-        cls.node2.session.delete(cls.node2.node)
-        cls.node1.stop()
-        cls.node2.stop()
+        self.db_conn.execute("commit")
+        try:
+            self.db_conn.execute("drop database test_node1")
+        except Exception, e:
+            print e
+
+        self.db_conn.execute("commit")
+        try:
+            self.db_conn.execute("drop database test_node2")
+        except Exception, e:
+            print e
+
+        self.db_conn.close()
